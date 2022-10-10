aa578c447464af81617d1a0b3136736dc56d4cb1
Riccardo <lupinu.ric@gmail.com>
2022-10-10 16:32:52 +0200

    Fix: use dependency injection on show CRUD

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -59,9 +59,8 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        $post = Post::findOrFail($id);
         return view('admin.posts.show', compact('post'));
     }
 
